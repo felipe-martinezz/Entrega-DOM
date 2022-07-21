3e4e5b113c0162d1353d93fcd5f64e2e00c74863
Felipe Martinez <felimartinez01@hotmail.com>
2022-07-21 16:39:41 -0300

    se complementa la entrega de EVENTOS con la de DOM

--- script.js
+++ script.js
@@ -7,14 +7,17 @@ class Usuario{
         this.pais = pais
     }
     
-    }
+}
     
     const usuarios = []
     
     const form = document.getElementById("idForm")
+    const divUsers = document.getElementById("divUsers")
+    const botonUsers = document.getElementById("botonUsers")
     
     form.addEventListener("submit", (event) => {
     event.preventDefault()
+
     let nombre = document.getElementById("idNombre").value
     let email= document.getElementById("idEmail").value
     let constraseña = document.getElementById("idContraseña").value
@@ -24,4 +27,21 @@ class Usuario{
     const usuario = new Usuario(nombre, email, constraseña, numero, pais)
     usuarios.push(usuario)
     console.log(usuarios)
+
+    form.reset()
+})
+
+botonUsers.addEventListener('click', () => {
+    usuarios.forEach(Usuario => {
+        divUsers.innerHTML += `
+        <div class="card" style="width: 18rem; margin:3px;">
+            <div class="card-body">
+                <h5 class="card-title">${Usuario.nombre}</h5>
+                <p class="card-text">${Usuario.email}</p>
+                <p class="card-text">${Usuario.pais}</p>
+            </div>
+        </div>
+        
+        `
     })
+})
